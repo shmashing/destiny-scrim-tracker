b286fbbbca3d247db34fab59f640ce8dcb31a978
Kyle McFarland <kyle.mcfarland@zapproved.com>
2020-01-13 15:39:59 -0800

    Add ID Prefixes as static classes to make sure they remain consistent and if we need to change them we only have to change them in a single place

---
+++ Destiny.ScrimTracker.Logic/Models/ModelIDPrefixes.cs
@@ -0,0 +1,12 @@
+namespace Destiny.ScrimTracker.Logic.Models
+{
+    public static class ModelIDPrefixes
+    {
+        public const string Guardian = "gua";
+        public const string GuardianEfficiency = "eff";
+        public const string GuardianElo = "elo";
+        public const string GuardianMatchResult = "gmr";
+        public const string Match = "mat";
+        public const string MatchTeam = "tea";
+    }
+}
