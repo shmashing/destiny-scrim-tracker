85439c0a84b34aa8ab1104bce0fac1232c34c7fa
Kyle McFarland <kyle.mcfarland@zapproved.com>
2020-01-30 10:40:06 -0800

    FaceLift With Styles

--- Destiny.ScrimTracker.Api/Controllers/HomeController.cs
+++ Destiny.ScrimTracker.Api/Controllers/HomeController.cs
@@ -20,83 +20,10 @@ namespace Destiny.ScrimTracker.Api.Controllers
             _matchService = matchService;
             _guardianService = guardianService;
         }
-        
-        // GET
+
         public IActionResult Index()
         {
-            var matchResults = _matchService.GetMatchResults();
-            return View(matchResults);
-        }
-
-        [Route("match")]
-        public IActionResult AddMatchView()
-        {
             return View();
-        }
-        
-        [Route("add_match")]
-        public IActionResult CreateMatchForm([FromQuery] int numOfTeams, [FromQuery] int playersPerTeam)
-        {
-            var guardians = _guardianService.GetGuardians();
-            ViewData.Add("Guardians", guardians.OrderBy(g => g.Guardian.GamerTag).Select(g => g.Guardian.GamerTag));
-            ViewData.Add("NumberOfTeams", numOfTeams);
-            ViewData.Add("PlayersPerTeam", playersPerTeam);
-
-            var match = new CreateMatchFormModel();
-
-            var teams = new List<Team>();
-            for (var i = 0; i < numOfTeams; i++)
-            {
-                teams.Add(new Team());
-                teams[i].MatchResults = new List<MatchResult>();
-                for (var j = 0; j < playersPerTeam; j++)
-                {
-                    teams[i].MatchResults.ToList().Add(new MatchResult());
-                }
-            }
-
-            match.Teams = teams;
-            return View(match);
-        }
-
-        [HttpPost]
-        [Route("add_match")]
-        public IActionResult FormatRequestAndAddMatch([FromQuery] int numOfTeams, [FromQuery] int playersPerTeam)
-        {
-            var createMatchRequest = new CreateMatchRequest();
-
-            createMatchRequest.MatchType = Enum.Parse<MatchType>(Request.Form["MatchType"]);
-            var teams = new List<MatchTeam>();
-            for (var i = 0; i < numOfTeams; i++)
-            {
-                var team = new MatchTeam();
-                team.Name = Request.Form[$"Team[{i}].Name"];
-                team.TeamScore = int.Parse(Request.Form[$"Team[{i}].TeamScore"]);
-                
-                var guardianResults = new List<GuardianMatchResult>();
-                for (var j = 0; j < playersPerTeam; j++)
-                {
-                    var results = new GuardianMatchResult
-                    {
-                        Kills = int.Parse(Request.Form[$"Team[{i}].MatchResults[{j}].Kills"]),
-                        Assists = int.Parse(Request.Form[$"Team[{i}].MatchResults[{j}].Assists"]),
-                        Deaths = int.Parse(Request.Form[$"Team[{i}].MatchResults[{j}].Deaths"]),
-                        GuardianName = Request.Form[$"Team[{i}].MatchResults[{j}].GuardianName"]
-                    };
-
-                    results.Efficiency = (double) (results.Kills + results.Assists) / results.Deaths;
-                    Console.WriteLine($"Calculated Eff = {results.Efficiency:F4}");
-                    guardianResults.Add(results);
-                }
-
-                team.GuardianMatchResults = guardianResults;
-                teams.Add(team);
-            }
-
-            createMatchRequest.Teams = teams;
-
-            _matchService.CreateMatch(createMatchRequest.ToMatch(), createMatchRequest.Teams);
-            return RedirectToAction("Index");
         }
     }
 }
--- Destiny.ScrimTracker.Api/Startup.cs
+++ Destiny.ScrimTracker.Api/Startup.cs
@@ -1,13 +1,18 @@
 using System;
+using System.Threading.Tasks;
 using Destiny.ScrimTracker.Logic.Adapters;
 using Destiny.ScrimTracker.Logic.Repositories;
 using Destiny.ScrimTracker.Logic.Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 
 namespace Destiny.ScrimTracker.Api
 {
@@ -42,8 +47,70 @@ namespace Destiny.ScrimTracker.Api
             services.AddTransient<IGuardianService, GuardianService>();
             services.AddTransient<IMatchService, MatchService>();
             
+            // ASP.NET Registrations
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+            
+            services.AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                })
+                .AddCookie()
+                .AddOpenIdConnect("Auth0", options => {
+                    // Set the authority to your Auth0 domain
+                    options.Authority = $"https://{Configuration["Auth0:Domain"]}";
+
+                    // Configure the Auth0 Client ID and Client Secret
+                    options.ClientId = Configuration["Auth0:ClientId"];
+                    options.ClientSecret = Configuration["Auth0:ClientSecret"];
+
+                    // Set response type to code
+                    options.ResponseType = OpenIdConnectResponseType.Code;
+
+                    // Configure the scope
+                    options.Scope.Add("openid");
+
+                    // Set the callback path, so Auth0 will call back to http://localhost:3000/callback
+                    // Also ensure that you have added the URL as an Allowed Callback URL in your Auth0 dashboard
+                    options.CallbackPath = new PathString("/callback");
+
+                    // Configure the Claims Issuer to be Auth0
+                    options.ClaimsIssuer = "Auth0";
+
+                    options.Events = new OpenIdConnectEvents
+                    {
+                        // handle the logout redirection
+                        OnRedirectToIdentityProviderForSignOut = (context) =>
+                        {
+                            var logoutUri = $"https://{Configuration["Auth0:Domain"]}/v2/logout?client_id={Configuration["Auth0:ClientId"]}";
+
+                            var postLogoutUri = context.Properties.RedirectUri;
+                            if (!string.IsNullOrEmpty(postLogoutUri))
+                            {
+                                if (postLogoutUri.StartsWith("/"))
+                                {
+                                    // transform to absolute
+                                    var request = context.Request;
+                                    postLogoutUri = request.Scheme + "://" + request.Host + request.PathBase + postLogoutUri;
+                                }
+                                logoutUri += $"&returnTo={ Uri.EscapeDataString(postLogoutUri)}";
+                            }
+
+                            context.Response.Redirect(logoutUri);
+                            context.HandleResponse();
+
+                            return Task.CompletedTask;
+                        }
+                    };
+                });
+            
             services.AddControllers();
-            services.AddMvc();
+            services.AddMvc(options => options.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,14 +120,18 @@ namespace Destiny.ScrimTracker.Api
             {
                 app.UseDeveloperExceptionPage();
             }
-            
+
+            app.UseStaticFiles();
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
+            app.UseMvc(routes =>
             {
-                endpoints.MapControllers();
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
 
